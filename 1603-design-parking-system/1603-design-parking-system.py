eb939d257d63f44d116764a0b55ac7ff5703f924
J-dabin <b00n0ng@naver.com>
2022-02-22 23:38:40 +0900

    Time: 226 ms (27.59%), Space: 14.5 MB (61.54%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -0,0 +1,26 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.big=big
+        self.medium=medium
+        self.small=small
+
+    def addCar(self, carType: int) -> bool:
+        if carType==1:
+            if self.big<1:
+                return False
+            else:
+                self.big-=1
+                return True
+        elif carType==2:
+            if self.medium<1:
+                return False
+            else:
+                self.medium-=1
+                return True
+        elif carType==3:
+            if self.small<1:
+                return False
+            else:
+                self.small-=1
+                return True
