b23da6a485082c37771dcda721a82871d7270437
Dabin Jeong <allempti@gmail.com>
2022-03-22 15:41:50 +0900

    Delete 1832-check-if-the-sentence-is-pangram directory

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def checkIfPangram(self, sentence: str) -> bool:
-        cnt = 0
-        for i in range(97, 123):
-            if chr(i) in sentence:
-                cnt += 1
-        return cnt == 26
