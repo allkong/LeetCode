d0509db80861f59bf90df9667e65fc4a64549dad
Dabin Jeong <allempti@gmail.com>
2022-05-05 00:58:12 +0900

    Delete 168-excel-sheet-column-title directory

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def convertToTitle(self, columnNumber: int) -> str:
-        str = ""
-        while columnNumber > 0:
-            columnNumber -= 1
-            str += chr(columnNumber % 26 + 65)
-            columnNumber //= 26
-        return str[::-1]
