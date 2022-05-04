dade95cbaf6e13ee477430752bdd57035da86249
Dabin Jeong <allempti@gmail.com>
2022-05-05 01:15:50 +0900

    Time: 38 ms (56.31%), Space: 13.9 MB (57.45%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.py
@@ -1,8 +1,8 @@
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
-        str = ""
+        s = ""
         while columnNumber > 0:
             columnNumber -= 1
-            str += chr(columnNumber % 26 + 65)
+            s += chr(columnNumber % 26 + 65)
             columnNumber //= 26
-        return str[::-1]
+        return s[::-1]
